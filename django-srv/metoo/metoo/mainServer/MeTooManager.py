d998ae563534fdd041015544dfa3b27b9983a915
bekas <bekas@bekas-PC.(none)>
2012-12-01 15:20:21 +0400

    write metoomanager

--- django-srv/metoo/metoo/mainServer/MeTooManager.py
+++ django-srv/metoo/metoo/mainServer/MeTooManager.py
@@ -14,10 +14,29 @@ class MeTooManager:
 	Класс, позволяющиййй узнать, кто идет на конкретное событие,\n
 	пойти самому, изменить тип похода или отказаться от события
 	'''
-	def getUsersbyEvent(eventId):
+	def getUsersbyEvent(sessionId,eventId):
 		'''
 		Узнать, кто идет на конкретное событие
 		'''
+		result = {}
+		userId = SessionManager.getUser(sessionId)
+		if(userId != -1):
+			if(MeToo.objects.get(userId=userId,eventId = eventId).exist):
+				metoo = MeToo.objects.filter(eventId = eventId)
+				result['list'] = []
+				for met in metoo:
+					addUser = {}
+					addUser['id'] = met.userId.pk
+					addUser['login'] = met.userId.login
+					addUser['avatar'] = met.userId.avatarId.photo
+					addUser['rating'] = met.userId.rating
+					result['list'].append(addUser)
+				result['result'] = 500
+			else:
+				result['result'] = 502
+		else:
+			result['result'] = 501
+		return result
 		
 	def meToo(sessionId,eventId,metooTypeId):
 		'''
@@ -27,13 +46,33 @@ class MeTooManager:
 		if(userId != -1):
 			metoo = MeToo(userId=userId,eventId = eventId, metooTypeId = metooTypeId)
 			metoo.save()
+			result = 500
+		else:
+			result = 501
+		return result
 		
 	def delMeToo(sessionId,eventId):
 		'''
 		Отказаться от события
 		'''
+		userId = SessionManager.getUser(sessionId)
+		if(userId != -1):
+			metoo = MeToo.objects.get(userId=userId,eventId = eventId)
+			metoo.delete()
+			result = 500
+		else:
+			result = 501
+		return result
+		
 	def modMeToo(sessionId,eventId,metooTypeId):
 		'''
 		Изменить тип похода
 		'''
-		
+		userId = SessionManager.getUser(sessionId)
+		if(userId != -1):
+			metoo = MeToo.objects.get(userId=userId,eventId = eventId)
+			metoo.metooTypeId = metooTypeId
+			result = 500
+		else:
+			result = 501
+		return result
